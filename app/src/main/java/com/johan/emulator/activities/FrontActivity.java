243554e29e1aebaaa848750952586bd5e89f34de
Johan Steenkamp <johan@johan-and>
2016-10-16 17:27:39 +0200

    modified:   app/src/main/java/com/johan/emulator/activities/DebugActivity.java 	modified:   app/src/main/java/com/johan/emulator/activities/FrontActivity.java 	renamed:    app/src/main/java/com/johan/emulator/activities/DebugActivity.java -> app/src/main/java/com/johan/emulator/engine/Emu6502.java 	modified:   app/src/main/res/menu/menu_main.xml

--- app/src/main/java/com/johan/emulator/activities/FrontActivity.java
+++ app/src/main/java/com/johan/emulator/activities/FrontActivity.java
@@ -1,6 +1,7 @@
 package com.johan.emulator.activities;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -8,8 +9,19 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 import com.johan.emulator.R;
+import com.johan.emulator.engine.Emu6502;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class FrontActivity extends AppCompatActivity {
+    Timer timer;
+    TimerTask timerTask;
+
+    private boolean running = false;
+    final Handler handler = new Handler();
+    private Emu6502 emuInstance = Emu6502.getInstance(getResources().getAssets());
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +38,44 @@ public class FrontActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+    }
+
+    @Override
+    protected  void onResume() {
+        super.onResume();
+        running = true;
+        timer = new Timer();
+        //breakAddress = getBreakAddress();
+
+        timerTask = new TimerTask() {
+            @Override
+            public void run() {
+                //System.out.println("In Beginning: " + System.currentTimeMillis());
+                final int result = emuInstance.runBatch(1);
+                //System.out.println("In End: " + System.currentTimeMillis());
+                if (result > 0) {
+                    handler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            timer.cancel();
+                            doAlert(result);
+                        }
+                    });
+                } else if (!running | (result < 0)) {
+                    handler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            timer.cancel();
+                            refreshControls();
+                        }
+                    });
+
+                }
+            }
+        };
+
+        timer.schedule(timerTask, 20, 20);
+
     }
 
 }
