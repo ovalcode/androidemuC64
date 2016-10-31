1aaf799622646402a5367d096ef78d75d6062679
Johan Steenkamp <johan@johan-and>
2016-10-31 20:34:02 +0200

    modified:   app/src/main/jni/timer.c

--- app/src/main/jni/timer.c
+++ app/src/main/jni/timer.c
@@ -2,3 +2,14 @@
 // Created by johan on 2016/10/31.
 //
 
+#include <alarm.h>
+
+
+void expired(struct timer_struct *tdev) {
+  tdev->started=0;
+}
+
+struct timer_struct getTimerInstance() {
+  struct timer_struct mytimer;
+  mytimer.expiredevent = &expired;
+}
